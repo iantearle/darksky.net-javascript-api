8c308cddce1e56fa96baca4de49ba4531087cfa4
Ian Tearle <ian@iantearle.com>
2013-04-18 18:11:47 +0200

    forecast.io proxy

---
+++ proxy.php
@@ -0,0 +1,13 @@
+<?php
+// File Name: proxy.php
+
+$api_key = 'API_KEY';
+
+$API_ENDPOINT = 'https://api.forecast.io/forecast/';
+$url = $API_ENDPOINT . $api_key . '/';
+
+if(!isset($_GET['url'])) die();
+$url = $url . urldecode($_GET['url']);
+$url = file_get_contents($url);
+
+print_r($url);
