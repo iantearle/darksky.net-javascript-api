9ddd44486caea2c871f6e0c7899a9f8dc4081157
Ian Tearle <ian@iantearle.com>
2015-02-05 20:35:17 +0000

    Closes #5

--- proxy.php
+++ proxy.php
@@ -7,7 +7,7 @@ $API_ENDPOINT = 'https://api.forecast.io/forecast/';
 $url = $API_ENDPOINT . $api_key . '/';
 
 if(!isset($_GET['url'])) die();
-$url = $url . urldecode($_GET['url']);
+$url = $url . $_GET['url'];
 $url = file_get_contents($url);
 
 print_r($url);
